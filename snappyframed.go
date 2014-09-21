591f33d57d5c13e11eb04edda1313bc5a6fb6665
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-09-20 22:22:53 -0700

    fix godoc header

--- snappyframed.go
+++ snappyframed.go
@@ -1,7 +1,10 @@
-// Package snappyframed implements reading and writing of snappy frammed compressed streams.
-//
-// See the snappy repository for details on the framing format.
-//	https://snappy.googlecode.com/svn/trunk/framing_format.txt
+/*
+Package snappyframed implements reading and writing of snappy framed compressed
+streams.
+
+See the snappy repository for details on the framing format.
+https://snappy.googlecode.com/svn/trunk/framing_format.txt
+*/
 package snappyframed
 
 import (
